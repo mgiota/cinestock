faedd8a0e321f792d7b9ad18b18213ccd2713a74
VeronikaHar <veronika.har@hotmail.com>
2019-04-19 22:54:06 -0500

    CardDeck Bootstrap component added.

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -2,22 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onClick } = this.props;
 
     return (
-      <Card style={{ width: "16rem" }}>
-        <Card.Img variant="top" src={movie.ImagePath} />
-        <Card.Body>
-          <Card.Title>{movie.Title}</Card.Title>
-          <Card.Text>{movie.Description}</Card.Text>
-          <Button onClick={() => onClick(movie)} variant="link">
-            Read more...
-          </Button>
-        </Card.Body>
-      </Card>
+      <CardDeck>
+        <Card style={{ width: "16rem" }}>
+          <Card.Img variant="top" src={movie.ImagePath} />
+          <Card.Body>
+            <Card.Title>{movie.Title}</Card.Title>
+            <Card.Text>{movie.Description}</Card.Text>
+            <Button onClick={() => onClick(movie)} variant="link">
+              Read more...
+            </Button>
+          </Card.Body>
+        </Card>
+      </CardDeck>
     );
   }
 }
