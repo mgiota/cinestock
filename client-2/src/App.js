3b933a65d5c3ab046f12833fc734b8eb4793fd86
veronikahar <veronika.har@hotmail.com>
2019-05-08 15:41:33 -0500

    Application state moved to Redux store.

--- client-2/src/App.js
+++ client-2/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import {
-  MainView
-} from './components/main-view/main-view';
-import './App.css';
+import React, { Component } from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-function App() {
-  return ( <
-    MainView / >
-  );
+import moviesApp from "./reducers/reducers";
+import MainView from "./components/main-view/main-view";
+
+import "./App.css";
+
+const store = createStore(moviesApp);
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <MainView />
+      </Provider>
+    );
+  }
 }
 
 export default App;
