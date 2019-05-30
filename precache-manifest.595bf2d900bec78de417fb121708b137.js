63ed27601722dfa06d3fc182e67be129e3f515be
mgiota <giota85@gmail.com>
2019-05-30 11:58:21 +0200

    Updates

---
+++ precache-manifest.595bf2d900bec78de417fb121708b137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd87d0e111240a2ed22d4ee4145bb3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "920112947132040fe229",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "17bd85efb9633f04efac",
+    "url": "/static/css/main.1a92cd70.chunk.css"
+  },
+  {
+    "revision": "920112947132040fe229",
+    "url": "/static/js/2.e9a83650.chunk.js"
+  },
+  {
+    "revision": "17bd85efb9633f04efac",
+    "url": "/static/js/main.11a612ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
