9a2d40888924d8d49ee384c93dd7a10feabaae88
mgiota <giota85@gmail.com>
2019-05-30 16:40:57 +0200

    Updates

--- precache-manifest.22274c1833195c64b1be9980bf2134b8.js
+++ precache-manifest.22274c1833195c64b1be9980bf2134b8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7849cb6e847034ac4785055eaa11d434",
+    "revision": "f55800a5902a41e37f03e3b469cb7569",
     "url": "/cinestock/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cinestock/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "41b00d95343de2a8d94b",
+    "revision": "d42e7188959ec5d432c5",
     "url": "/cinestock/static/css/main.1a92cd70.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cinestock/static/js/2.e9a83650.chunk.js"
   },
   {
-    "revision": "41b00d95343de2a8d94b",
-    "url": "/cinestock/static/js/main.3cdd6a27.chunk.js"
+    "revision": "d42e7188959ec5d432c5",
+    "url": "/cinestock/static/js/main.e99f9a25.chunk.js"
   },
   {
     "revision": "9ed1b90631ff7ec3e0fb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/cinestock/precache-manifest.2d732971184b7863516c3264cf0b59da.js"
+  "/cinestock/precache-manifest.22274c1833195c64b1be9980bf2134b8.js"
 );
 
 self.addEventListener('message', (event) => {
